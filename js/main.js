733f106d2b864604d30c9f3e7c8190ccc2c540ec
Royce Whitaker <royce@rdw.io>
2017-10-03 20:29:06 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ jQuery(document).ready(function(event){
         //wait for the end of the transition on the loading bar before revealing the new content
         ( section.hasClass('cd-about') ) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
 	( section.hasClass('cd-bible') ) ? $('body').addClass('cd-bible') : $('body').removeClass('cd-bible');
-	( section.hasClass('cd-two') ) ? $('body').addClass('cd-two') : $('body').removeClass('cd-two');
+	( section.hasClass('cd-posts') ) ? $('body').addClass('cd-posts') : $('body').removeClass('cd-posts');
 	( section.hasClass('cd-three') ) ? $('body').addClass('cd-three') : $('body').removeClass('cd-three');
 	( section.hasClass('cd-four') ) ? $('body').addClass('cd-four') : $('body').removeClass('cd-four');
 	( section.hasClass('cd-five') ) ? $('body').addClass('cd-five') : $('body').removeClass('cd-five');
